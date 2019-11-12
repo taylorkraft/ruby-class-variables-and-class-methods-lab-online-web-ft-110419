c7e24473bab75770ee709404cdf3d94ee37e1278
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-12 23:43:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,21 +2,15 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0
+  @@artists
+  @@genres 
   
   def initialize(name, artist, genre)
-    @@count += 1 
+    @@count += 1
   end
   
   def self.count
     @@count
-  end
-  
-  def self.name
-    @@name
-  end
-  
-  def self.artists
-    @@artists
   end
   
 end
