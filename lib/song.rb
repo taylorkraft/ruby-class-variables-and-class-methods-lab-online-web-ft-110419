b486895d31c1ac81b9691896b798b2ca946c5e61
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-13 00:12:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,16 +23,16 @@ class Song
     @@genres.uniq
   end
   
+  def self.genre_count
+  end
+
   @@artists = []
   
   def self.artists
     @@artists.uniq
   end
   
-  def self.genre_count
-end
-
-def self.artist_count
-end
+  def self.artist_count
+  end
   
 end
